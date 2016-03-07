6af0896439083020feb0e72499923c3707b4d88a
Marcelo Calbucci <marcelo@calbucci.com>
2016-03-07 15:04:24 -0800

    Routing

---
+++ RouteNaive/source/RepoDetails.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import 'whatwg-fetch';
+
+class RepoDetails extends Component{
+    constructor(){
+        super(...arguments);
+        this.state = { repository: [] };
+    }
+    
+    fetchData(repo_name){
+        fetch('https://api.github.com/repos/calbucci/' + repo_name)
+        .then((response) => response.json())
+        .then((responseData) => {
+            this.setState({repository: responseData});
+        });
+    }
+    
+    componentDidMount(){
+        let repo_name = this.props.params.repo_name;
+        this.fetchData(repo_name);
+    }
+    
+    componentWillReceiveProps(nextProps){
+        let repo_name = nextProps.params.repo_name;
+        this.fetchData(repo_name);
+    }
+    
+    render(){
+        let stars = [];
+        for(var i = 0; i < this.state.repository.stargazers_count; i++){
+            stars.push('*');
+        }
+        
+        return (
+            <div>
+                <h2>{this.state.repository.name}</h2>
+                <p>{this.state.repository.description}</p>
+                <span>{stars}</span>
+            </div>  
+        );
+    }
+}
+
+export default RepoDetails;
---
+++ RouteNaive/source/app.jsx
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import About from './about';
+import Home from './home';
+import Repos from './repos';
+import RepoDetails from './RepoDetails';
+import { Router, Route, Link, IndexRoute } from 'react-router';
+
+
+class App extends Component{
+    // constructor(){
+    //     super(...arguments);
+    //     this.state = {
+    //         route: window.location.hash.substr(1)
+    //     };
+    // }
+    
+    // componentDidMount(){
+    //     window.addEventListener('hashchange', () =>{
+    //         this.setState({
+    //             route: window.location.hash.substr(1)
+    //         });
+    //     });
+    // }
+    
+    render(){
+        // var Child;
+        // console.log("route=" + this.state.route);
+        // switch(this.state.route){
+        //     case '/about': Child = About; break;
+        //     case '/repos': Child = Repos; break;
+        //     default: Child = Home; break;
+        // }
+        // 
+        // console.log(Child);
+        
+        return (
+            <div>
+                <header>App</header>
+                <menu>
+                    <ul>
+                        <li><Link to='/about'>About</Link></li>
+                        <li><Link to='/repos'>Repos</Link></li>
+                    </ul>
+                </menu>
+                {this.props.children}
+            </div>
+            
+        );
+    }
+    
+}
+
+render(
+    <Router>
+        <Route path="/" component={App}>
+            <IndexRoute component={Home} />
+            <Route path="about" component={About} />
+            <Route path="repos" component={Repos} >
+                <Route path="details/:repo_name" component={RepoDetails} />
+            </Route>
+        </Route>
+    </Router>
+    , document.getElementById('root'));
